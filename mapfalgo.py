a09d7db7aa2517876b02ba50d543dfc67262dd6e
Isseinie <isseinie.calviac@ens-rennes.fr>
2021-05-27 12:04:01 +0200

    graphml en entrée

--- mapfalgo.py
+++ mapfalgo.py
@@ -1,58 +1,59 @@
 import pysat
 from pysat.solvers import Solver
+from igraph import *
 
 NMAX = 800
 
-def decoupled_exec(V, Ec, Em, A) :
+def decoupled_exec(G_M, G_C, A) :
     return [[] for a in A]
 
 def has_conflict(P) :
     return False
 
-def is_connected(V, Ec, Em, A, agent, t):
+def is_connected(G_M, G_C, A, agent, t):
     return True
 
-def pick_disconnected(V, Ec, Em, A):
+def pick_disconnected(G_M, G_C, A):
     return A[0], 0
 
-def find_path(V, Ec, Em, A, n, constraints):
+def find_path(G_M, G_C, A, n, constraints):
     return Solver()
 
-def extract_exec(V, Ec, Em, A, constraints):
+def extract_exec(G_M, G_C, A, constraints):
     return [[] for a in A]
 
-def pick_config(V, Ec, Em, A, t):
+def pick_config(G_M, G_C, A, t):
     return [[0] for a in A]
 
-def MAPF1(V, Ec, Em, A) :
+def MAPF1(G_M, G_C, A) :
     constraints = []
-    P = decoupled_exec(V, Ec, Em, A)
+    P = decoupled_exec(G_M, G_C, A)
     n = max([len(Pai) for Pai in P])
     while has_conflict(P):
-        agent, t = pick_disconnected(V, Ec, Em, A)
-        constraints.append(is_connected(V, Ec, Em, A, agent, t))
-        while(find_path(V, Ec, Em, A, n, constraints).solve() == False) :
+        agent, t = pick_disconnected(G_M, G_C, A)
+        constraints.append(is_connected(G_M, G_C, A, agent, t))
+        while(find_path(G_M, G_C, A, n, constraints).solve() == False) :
             n+= 1
-        P = extract_exec(V, Ec, Em, A, constraints)
+        P = extract_exec(G_M, G_C, A, constraints)
     return P
 
-def MAPFmalin(V, Ec, Em, A, final_state):
+def MAPFmalin(G_M, G_C, A, final_state):
     constraints = []
-    P = decoupled_exec(V, Ec, Em, A)
+    P = decoupled_exec(G_M, G_C, A)
     n = max([len(Pai) for Pai in P])
     if not(has_conflict(P)) :
         return P
     else :
-        agent, t = pick_disconnected(V, Ec, Em, A)
-        constraints.append(is_connected(V, Ec, Em, A, agent, t))
+        agent, t = pick_disconnected(G_M, G_C, A)
+        constraints.append(is_connected(G_M, G_C, A, agent, t))
         while n < NMAX :
-            while(find_path(V, Ec, Em, A, n, constraints).solve() == False) :
+            while(find_path(G_M, G_C, A, n, constraints).solve() == False) :
                 n+= 1
-            P_extr = extract_exec(V, Ec, Em, A, n, constraints)
-            P_final = MAPFmalin(V, Ec, Em, A, P_extr[-1]) + MAPFmalin(V, Ec, Em, P_extr[-1], final_state)
+            P_extr = extract_exec(G_M, G_C, A, n, constraints)
+            P_final = MAPFmalin(G_M, G_C, A, P_extr[-1]) + MAPFmalin(G_M, G_C, P_extr[-1], final_state)
             if not(has_conflict(P_final)) :
                 return P_final
             else :
-                constraints.append(pick_config(V, Ec, Em, A, t) != P_extr[t])
+                constraints.append(pick_config(G_M, G_C, A, t) != P_extr[t])
 
 

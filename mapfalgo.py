6b038a285fb1e35d3c56bcff3e1ea12c385f6272
Isseinie <isseinie.calviac@ens-rennes.fr>
2021-05-31 10:05:18 +0200

    nouvel algo

--- mapfalgo.py
+++ mapfalgo.py
@@ -5,14 +5,41 @@ from igraph import *
 def decoupled_exec(G_M, G_C, A) :
     return 0
 
-def has_conflict(P) :
-    return False
+def nb_conflicts(P) :
+    return 0
 
-def is_connected(G_M, G_C, A, agent, t, P):
+def is_connected(G_C, A, i, t, P): #a_i est connecté aux agents a_0... a_i-1 au temps t 
     return True
 
 def last_is_disconnected(G_M, G_C, A, P):
     return False, 0
+
+def pick_time_with_conflict(P) :
+    return len(P)//2
+
+def choose_order(A) :
+    return A
+
+def choose_best_neighbour(A, i, t, P):
+    return 0 #voisin des a_0...a_i-1 tq chemin de longueur t de s_i à u, minimise d(u, g_i) et le nb de conflits 
+
+def update(P, u, i):
+    return P #update P avec a_i qui passe par u
+
+def mapfdichotomie(G_M, G_C, A):
+    P = decoupled_exec(G_M, G_C, A)
+    t = pick_time_with_conflict(P)
+    A_ordered = choose_order(A)
+    nb_it = 5
+    while nb_it > 0 :
+        P_changed = P
+        for i in range(1, len(A)):
+            if not(is_connected(G_C, A_ordered, i, t, P_changed)):
+                u = choose_best_neighbour(A, i, t, P_changed)
+                update(P_changed, u, i) #màj de P_i
+        if nb_conflicts(P_changed) < nb_conflicts(P):
+            P = P_changed
+        nb_it -= 1
 
 '''def find_path(G_M, G_C, A, n, constraints):
     return Solver()
